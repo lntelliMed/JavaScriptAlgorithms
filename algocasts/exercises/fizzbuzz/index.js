f6cae23a230ef03ba4b80ac90ac2f0451230fbdd
“Sam <sam.alsmadi@gmail.com>
2018-03-30 17:30:10 -0400

    Add solution for fizzbuzz

--- algocasts/exercises/fizzbuzz/index.js
+++ algocasts/exercises/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// Attempt #1
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
