f2a242dc8935d7251f1e99aa7bbd30ab645b1528
“Sam <sam.alsmadi@gmail.com>
2018-03-30 17:10:12 -0400

    Add maxchar  solutions

--- algocasts/exercises/maxchar/index.js
+++ algocasts/exercises/maxchar/index.js
@@ -5,6 +5,39 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Attempt #1
+// function maxChar(str) {
+//   const charCounts = {};
+//   for (let i = 0; i < str.length; i++) {
+//     if (!charCounts[str[i]]) {
+//       charCounts[str[i]] = 1;
+//     } else {
+//       charCounts[str[i]]++;
+//     }
+//   }
+
+//   const sortedChars = Object.keys(charCounts).sort((a, b) => charCounts[a] > charCounts[b]);
+//   return sortedChars[sortedChars.length - 1];
+// }
+
+// Attempt #2
+function maxChar(str) {
+  const charCounts = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charCounts[char] = charCounts[char] + 1 || 1;
+  }
+
+  for (let char in charCounts) {
+    if (charCounts[char] >= max) {
+      max = charCounts[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
