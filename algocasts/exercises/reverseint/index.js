c89e73f64cdf49f2e44dc3763dda27b88f1650f9
“Sam <sam.alsmadi@gmail.com>
2018-03-30 02:46:29 -0400

    Add reverseint solutions

--- algocasts/exercises/reverseint/index.js
+++ algocasts/exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Attempt #1
+// function reverseInt(n) {
+//   if (n === 0) return n;
+//   let sign = Math.sign(n);
+//   n = sign * n;
+//   let reversedNumber = parseInt(n.toString().split('').reverse().join(''));
+//   return sign * reversedNumber;
+// }
+
+// Attempt #2
+function reverseInt(n) {
+  if (n === 0) return n;
+  let sign = Math.sign(n);
+  n = sign * n;
+  let reversedNumber = 0;
+  while (n > 0) {
+    let rightDigit = n % 10;
+    reversedNumber = (reversedNumber * 10) +rightDigit;
+    n = Math.floor(n / 10);
+  }
+  return sign * reversedNumber;
+}
 
 module.exports = reverseInt;
