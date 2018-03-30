b5582d83edefa23dfcfc6eacff5e10696396463a
“Sam <sam.alsmadi@gmail.com>
2018-03-29 20:17:18 -0400

    Add palindrome solutions

--- algocasts/exercises/palindrome/index.js
+++ algocasts/exercises/palindrome/index.js
@@ -7,6 +7,64 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Attemp #1
+// function reverse(str) {
+//   if (str === null || str.length === 0) {
+//     return str;
+//   }
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+// function palindrome(str) {
+//   if (str === null || str.length === 0) {
+//     return true;
+//   }
+//   if (str === reverse(str)) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// Attempt #2
+// function palindrome(str) {
+//   if (str === null || str.length === 0) {
+//     return true;
+//   }
+
+//   return str === str.split('').reverse().join('');
+// }
+
+// Attempt #4
+// function palindrome(str) {
+//   if (str === null || str.length === 0) {
+//     return true;
+//   }
+
+//   return str.split('').every((char, i) => char === str[str.length - 1 - i]);
+// }
+
+// Attempt #3
+function palindrome(str) {
+  if (str === null || str.length === 0) {
+    return true;
+  }
+
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
 
 module.exports = palindrome;
