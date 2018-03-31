20eadfe10d43f272130635cb51fe686367fed228
“Sam <sam.alsmadi@gmail.com>
2018-03-30 21:23:20 -0400

    Add solutions for chunk

--- algocasts/exercises/chunk/index.js
+++ algocasts/exercises/chunk/index.js
@@ -8,6 +8,49 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Attempt #1
+// function chunk(array, size) {
+//   let outer = [];
+//   let inner = [];
+//   let counter = 0;
+
+//   for (let i = 0; i < array.length; i++) {
+//     inner.push(array[i]);
+//     counter++;
+//     if (counter === size || i === array.length - 1) {
+//       outer.push(inner);
+//       counter = 0;
+//       inner = [];
+//     }
+//   }
+//   return outer;
+// }
+
+// Attempt #2
+// function chunk(array, size) {
+//   let outer = [];
+//   for (let element of array) {
+//     let lastInner = outer[outer.length - 1];
+//     if (!lastInner || lastInner.length === size) {
+//       outer.push([element]);
+//     } else {
+//       lastInner.push(element);
+//     }
+//   }
+//   return outer;
+// }
+
+// Attempt #3
+function chunk(array, size) {
+  let outer = [];
+  let start = 0;
+  while (start < array.length) {
+    outer.push(array.slice(start, start + size));
+    start += size;
+  }
+  return outer;
+}
+
+
 
 module.exports = chunk;
