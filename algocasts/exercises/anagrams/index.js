fdf910f35a8e65043bd3b02de8c8adffa8333352
“Sam <sam.alsmadi@gmail.com>
2018-03-30 22:20:52 -0400

    Add solutions for anagrams

--- algocasts/exercises/anagrams/index.js
+++ algocasts/exercises/anagrams/index.js
@@ -8,6 +8,71 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Attempt #1
+// function anagrams(stringA, stringB) {
+//   stringA = stringA.toLowerCase().replace(' ', '').replace('!', '');
+//   stringB = stringB.toLowerCase().replace(' ', '').replace('!', '');
+
+//   if (stringA.length !== stringB.length) {
+//     return false;
+//   }
+//   let stringAChars = {};
+//   let stringBChars = {};
+
+//   for (let char of stringA) {
+//     if (!stringAChars[char]) {
+//       stringAChars[char] = 1;
+//     } else {
+//       stringAChars[char]++;
+//     }
+//   }
+
+//   for (let char of stringB) {
+//     if (!stringBChars[char]) {
+//       stringBChars[char] = 1;
+//     } else {
+//       stringBChars[char]++;
+//     }
+//   }
+
+//   for (let char in stringAChars) {
+//     if (stringAChars[char] !== stringBChars[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+
+// Attempt #2
+// function buildCharMap (str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+//  }
+
+// function anagrams(stringA, stringB) {
+//   let aCharMap = buildCharMap(stringA);
+//   let bCharMap = buildCharMap(stringB);
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// Attempt #3
+function cleanUpString (str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+function anagrams(stringA, stringB) {
+  return cleanUpString(stringA) === cleanUpString(stringB);
+}
 
 module.exports = anagrams;
