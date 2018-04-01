af5c555f2b2f8fe1332ebabc82e7978c2564be3b
“Sam <sam.alsmadi@gmail.com>
2018-03-31 23:37:21 -0400

    Add solutions for capitalize

--- algocasts/exercises/capitalize/index.js
+++ algocasts/exercises/capitalize/index.js
@@ -7,6 +7,50 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Attemp #1
+// function capitalize(str) {
+//   let words = str.split(' ');
+//   let capitalizedWords = [];
+//   for (let word of words) {
+//     capitalizedWords.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return capitalizedWords.join(' ');
+// }
 
+// Attemp #2
+// function capitalize(str) {
+//   if (str === null || str.length === 0) {
+//     return str;
+//   }
+//   let finalStr = '';
+
+//   let prevChar = ' ';
+//   for (let char of str) {
+//     if (prevChar === ' ' && char !== ' ') {
+//       finalStr += char.toUpperCase();
+//     } else {
+//       finalStr += char
+//     }
+//     prevChar = char;
+
+//   }
+//   return finalStr;
+// }
+
+// Attempt #3
+function capitalize(str) {
+  if (str === null || str.length === 0) {
+    return str;
+  }
+  let finalStr = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      finalStr += str[i].toUpperCase();
+    } else {
+      finalStr += str[i];
+    }
+  }
+  return finalStr;
+}
 module.exports = capitalize;
