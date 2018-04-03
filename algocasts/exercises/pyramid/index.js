43d0941d0669627d851c11bb3f28424137f243cb
“Sam <sam.alsmadi@gmail.com>
2018-04-02 22:10:21 -0400

    Add solutions for pyramid

--- algocasts/exercises/pyramid/index.js
+++ algocasts/exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Attempt #1
+// function pyramid(n) {
+//   let mid = Math.floor((2 * n - 1) / 2);
+//   let out = ''
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < 2 * n - 1; j++) {
+//       if (j <= mid + i && j >= mid - i) {
+//         out += '#';
+//       } else {
+//         out += ' ';
+//       }
+//     }
+//     console.log(out);
+//     out = '';
+//   }
+// }
 
+// Attempt #2
+function pyramid(n, row = 0, str = '') {
+  let mid = Math.floor((2 * n - 1) / 2);
+  if (n === row) {
+    return;
+  }
+
+  if (str.length === (2 * n - 1)) {
+    console.log(str);
+    return pyramid(n, row + 1)
+  }
+
+  if (str.length <= (2 * n - 1)) {
+    if (str.length <= mid + row && str.length >= mid - row) {
+      str += '#';
+    } else {
+      str += ' ';
+    }
+  }
+  pyramid(n, row, str);
+}
 module.exports = pyramid;
