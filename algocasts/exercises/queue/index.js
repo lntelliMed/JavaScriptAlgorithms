a8f62286905370064b3251dbe458e38a8813a6bf
“Sam <sam.alsmadi@gmail.com>
2018-04-04 19:26:53 -0400

    Add few solutions for queue

--- algocasts/exercises/queue/index.js
+++ algocasts/exercises/queue/index.js
@@ -8,6 +8,55 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+// Attempt #1
+// class Queue {
+//   constructor () {
+//     this.queue = [];
+//   }
+
+//   add (val) {
+//     this.queue.unshift(val);
+//   }
+
+//   remove () {
+//     if (!this.queue.length) {
+//       return;
+//     }
+//     return this.queue.pop();
+//   }
+// }
+
+// Attempt #2
+// class Queue {
+//   constructor() {
+//     this.queue = [];
+//   }
+
+//   add(val) {
+//     this.queue.push(val);
+//   }
+
+//   remove() {
+//     if (!this.queue.length) {
+//       return;
+//     }
+//     return this.queue.shift();
+//   }
+// }
+
+// Attempt #3
+function Queue() {
+  this.queue = [];
+}
+
+Queue.prototype.add = function (val) {
+  this.queue.unshift(val);
+}
+
+Queue.prototype.remove = function () {
+  if (!this.queue.length) return;
+  return this.queue.pop();
+}
+
 
 module.exports = Queue;
