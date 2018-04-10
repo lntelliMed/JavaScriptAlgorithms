1d893b3b97e9d761a8b702c3318c3d3839b147eb
“Sam <sam.alsmadi@gmail.com>
2018-04-10 16:06:53 -0400

    Implement insert/contains for bst

--- algocasts/exercises/bst/index.js
+++ algocasts/exercises/bst/index.js
@@ -11,6 +11,70 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+  // Attempt #1
+  // insert (data, root = this) {
+  //   if (data < root.data) {
+  //     if (root.left === null) {
+  //       root.left = new Node(data);
+  //     } else {
+  //       return this.insert(data, root.left);
+  //     }
+  //   } else {
+  //     if (root.right === null) {
+  //       root.right = new Node(data);
+  //     } else {
+  //       return this.insert(data, root.right);
+  //     }
+  //   }
+  // }
+
+    // Attempt #2
+    insert (data) {
+      if (data < this.data && this.left) {
+        this.left.insert(data);
+      } else if (data < this.data) {
+        this.left = new Node(data);
+      } else if (data > this.data && this.right) {
+        this.right.insert(data);
+      } else if (data > this.data) {
+        this.right = new Node(data);
+      }
+    }
+
+
+    // Attempt #1
+    // contains (data, root = this) {
+    //   if (!root || data === root.data) {
+    //     return root;
+    //   } else if (data < root.data) {
+    //     return this.contains(data, root.left);
+    //   } else if (data > root.data) {
+    //     return this.contains(data, root.right);
+    //   }
+    // }
+
+    // Attempt #2
+    contains (data) {
+      if (this.data === data) {
+        return this;
+      }
+
+      if (data < this.data && this.left) {
+        return this.left.contains(data);
+      }
+
+      if (data > this.data && this.right) {
+        return this.right.contains(data);
+      }
+
+      return null;
+    }
+}
 
 module.exports = Node;
