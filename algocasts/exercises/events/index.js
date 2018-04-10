4a91923de7918bf1d93dcae02bae7130acfb5ce6
“Sam <sam.alsmadi@gmail.com>
2018-04-10 18:29:06 -0400

    Implement Events class

--- algocasts/exercises/events/index.js
+++ algocasts/exercises/events/index.js
@@ -4,16 +4,34 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor () {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [callback];
+    } else {
+      this.events[eventName].push(callback);
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let callback of this.events[eventName]) {
+        callback();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    // this.events[eventName] = [];
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
