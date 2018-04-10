60501011d1dc5794f712d66e1680086340911d3f
“Sam <sam.alsmadi@gmail.com>
2018-04-09 21:34:50 -0400

    Add solution for  levelwidth

--- algocasts/exercises/levelwidth/index.js
+++ algocasts/exercises/levelwidth/index.js
@@ -11,6 +11,49 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+/*
+  1
+ 2 3
+*/
+
+// Attempt #1
+// function levelWidth(root) {
+//   let queue = [root, null];
+//   let counts = [];
+//   let counter = 0;
+//   while (queue.length) {
+//     let node = queue.shift();
+//     if (node) {
+//       ++counter;
+//       queue.push(...node.children);
+//     } else {
+//       counts.push(counter);
+//       counter = 0;
+//       if (queue.length) {
+//         queue.push(null);
+//       }
+//     }
+//   }
+//   return counts;
+// }
+
+// Attempt #2
+function levelWidth(root) {
+  let queue = [root, 'end'];
+  let counts = [0];
+
+  while (queue.length > 1) {
+    let node = queue.shift();
+    if (node === 'end') {
+      counts.push(0);
+      queue.push('end');
+    } else {
+      counts[counts.length - 1]++;
+      queue.push(...node.children);
+    }
+  }
+
+  return counts;
+}
 
 module.exports = levelWidth;
