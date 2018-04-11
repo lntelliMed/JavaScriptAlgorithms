3f145dca5646817224206bb7ae263e920131eeae
“Sam <sam.alsmadi@gmail.com>
2018-04-11 01:49:48 -0400

    Implement bubble sort

--- algocasts/exercises/sorting/index.js
+++ algocasts/exercises/sorting/index.js
@@ -1,8 +1,33 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
 
+// Attempt #1
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// Attempt #1
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
