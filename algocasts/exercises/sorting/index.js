46d6196f3bb86c0d6734275da291172b254f1a95
“Sam <sam.alsmadi@gmail.com>
2018-04-11 21:53:47 -0400

    Implement merge sort

--- algocasts/exercises/sorting/index.js
+++ algocasts/exercises/sorting/index.js
@@ -47,11 +47,36 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  if (left.length) {
+    result.push(...left);
+  }
 
+  if (right.length) {
+    result.push(...right);
+  }
+
+
+  return result;
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
